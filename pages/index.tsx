844071c25b2fb76cbd40b5c2b8c021eff0e9d4cd
Warren Falk <warren@warrenfalk.com>
2025-03-06 08:36:51 -0500

    experiment, moving forward

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import { trpc } from "@/utils/trpc";
-
 export default function Home() {
-  const hello = trpc.hello.useQuery({ text: 'client' });
-  if (!hello.data) return <div>Loading...</div>;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>Hello World! {hello.data.greeting}</div>
+        <div>Hello World!</div>
+        <div>var: {process.env.NEXT_PUBLIC_TEST_VAR}</div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
