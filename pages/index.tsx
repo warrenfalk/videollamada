3722ff60847709ea5819e2a523517c9189cbd0f0
Warren Falk <warren@warrenfalk.com>
2025-03-06 08:41:10 -0500

    extra code change

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>Hello World!</div>
         <div>var: {process.env.NEXT_PUBLIC_TEST_VAR}</div>
+        <div>extra code change</div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
